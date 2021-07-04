bb67adfb349163032c2f8cc9125a4da2e2db0c93
Douglas Bueno Oliveira <dbuenool@everis.com>
2021-07-04 18:15:25 -0300

    criacao da documentacao em swagger

--- src/main/java/com/buenoponto/controledeponto/model/JornadaTrabalho.java
+++ src/main/java/com/buenoponto/controledeponto/model/JornadaTrabalho.java
@@ -1,16 +1,23 @@
 package com.buenoponto.controledeponto.model;
 
-import lombok.*;
-import org.hibernate.envers.Audited;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Getter
-@Setter
+import org.hibernate.envers.Audited;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @EqualsAndHashCode
 @Builder
 @Entity
